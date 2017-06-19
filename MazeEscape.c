4523934d98b143bf609e36e383a91b34a01886a1
Yu-Seong-Min <tjdalsdbqqq@naver.com>
2017-06-19 23:47:40 +0900

    print logo

--- MazeEscape.c
+++ MazeEscape.c
@@ -7,7 +7,7 @@
 //------------------------------------------------------------
 #define WIDE 57
 #define HIGH 29
-#define VISION 8
+#define VISION 4
 #define UP 72
 #define LEFT 75
 #define RIGHT 77
@@ -15,9 +15,12 @@
 //------------------------------------------------------------
 int i = 0, j = 0;
 char map[HIGH][WIDE];
+char logo[6][79];
 //------------------------------------------------------------
+void scan_logo();
+void print_UI(int menu);
 void scan_map();
-void print_map();
+void print_map(int cx, int cy);
 void move_character(int cx,int cy);
 void search_character(int *x, int*y);
 void check_clear(int *clear);
@@ -26,48 +29,99 @@ void gotoxy(int x, int y);
 //------------------------------------------------------------
 int main()
 {
-	RemoveCursor();
-
+	int select_menu = 0;
 	int clear = 0;
 	int character_y;
 	int character_x;
 
+	RemoveCursor();
 	scan_map();
 	while (clear != 1)
 	{
+		print_UI(select_menu);
 		search_character(&character_x, &character_y);
-		print_map();
-		move_character(character_x, character_y);
+		//print_map(character_x, character_y);
 		check_clear(&clear);
+		move_character(character_x, character_y);
 	}
 }
 //------------------------------------------------------------
+void scan_logo()
+{
+	FILE *logo_f = fopen("Logo.txt", "r");
+
+	for (i = 0;i < 6;i++)
+	{
+		for (j = 0;j < 79;j++)
+		{
+			fscanf(logo_f, "%c", &logo[i][j]);
+		}
+	}
+	fclose(logo_f);
+}
+void print_UI(int menu)
+{
+	if (menu == 0)
+	{
+		gotoxy(0, 5);
+		for (i = 0;i < 7;i++)
+		{
+			for (j = 0;j < 79;j++)
+			{
+				if (logo[i][j] == '0')
+					printf(" ");
+				else if (logo[i][j] == "1")
+					printf("#");
+			}
+		}
+	}
+	else if (menu == 1)
+	{
+		gotoxy(114, 23);
+		printf("¢¸ FINSH");
+		gotoxy(116, 2);
+		printf("How to play");
+		gotoxy(116, 3);
+		printf("¡è : Turn UP");
+		gotoxy(116, 4);
+		printf("¡ç : Turn LEFT");
+		gotoxy(116, 5);
+		printf("¡æ : Turn RIGHT");
+		gotoxy(116, 6);
+		printf("¡é : Turn DOWN");
+	}
+}
 void scan_map()
 {
-	FILE *fp = fopen("MazeMap1.txt", "r");
+	FILE *map_f = fopen("MazeMap1.txt", "r");
 
 	for (i = 0;i < HIGH;i++)
 	{
 		for (j = 0;j < WIDE;j++)
 		{
-			fscanf(fp, "%c", &map[i][j]);
+			fscanf(map_f, "%c", &map[i][j]);
 		}
 	}
-	fclose(fp);
+	fclose(map_f);
 }
-void print_map()
+void print_map(int cx, int cy)
 {
 	gotoxy(0, 0);
 	for (i = 0;i < HIGH;i++)
 	{
 		for (j = 0;j < WIDE;j++)
 		{
-			if (map[i][j] == '0')
+			if (cy + VISION > i&&cy - VISION<i&&cx + VISION>j&&cx - VISION < j)
+			{
+				if (map[i][j] == '0')
+					printf("  ");
+				else if (map[i][j] == '1')
+					printf("¡á");
+				else if (map[i][j] == '2')
+					printf("¿Ê");
+			}
+			else
 				printf("  ");
-			else if (map[i][j] == '1')
-				printf("¡á");
-			else if (map[i][j] == '2')
-				printf("¿Ê");
 		}
 		printf("\n");
 	}
